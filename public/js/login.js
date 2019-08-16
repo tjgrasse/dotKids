b2b2e93b0fc23083d919012f7492cedd02b8fec2
jknguyen22 <jen.nguyen916@gmail.com>
2019-08-15 19:58:53 -0500

    connect parent home to login

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+//check if form fields are not empty
+function notEmpty(formdata) {
+  if (formdata.value.length == 0){
+      console.log("Required field(s) are empty!");
+      return false;
+  }
+  console.log("Required fields are not empty.")
+  return true;
+}
+
+//check to make sure checkbox is checked
+function isChecked(checkbox){
+   if(checkbox.checked == false){
+      console.log("Checkbox is not checked!");
+      return false;
+   }
+   console.log("Checkbox is checked.");
+   return true;
+}
+
+// Check the form contents to make sure they are ok, currently confirming that the
+// two passwords match, that fields are not empty and that checkbox is checked.
+function checkFormContents() {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  var checkbox1 = document.getElementById("checkLogged");
+  var checkbox2 = document.getElementById("checkLoggedParent");
+
+  console.log(username.value);
+  console.log(password.value);
+  console.log(checkLogged.value);
+  console.log(checkLoggedParent.value);
+
+  if (notEmpty(password) == false ||  notEmpty(username) == false)
+  {
+      alert("Required field(s) are empty!");
+      return false;
+  }
+  if(isChecked(checkbox2) == false)
+  {
+    window.location.href = "../views/parenthome";
+  }
+  window.location.href = "/parenthome";
+}
--- server.js
+++ server.js
@@ -10,9 +10,10 @@ const http = require('http');
 const url = require('url');
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log('Setting port to default value of 3000');
 var port = 3000;
@@ -21,8 +22,26 @@ app.use('/views', express.static('views'));
 app.use('/public',express.static('public'));
 
 app.get('/', function(req, res) {
-	console.log('rendering home');
+	console.log('rendering login');
 	res.sendFile(path.join(__dirname + '/views/login.html'));
+});
+
+app.post('/login', function(req, res) {
+	console.log('Login User');
+	if(req.body.checkLoggedParent) {
+		res.sendFile(path.join(__dirname + '/views/parenthome.html'));
+	}
+	res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+
+app.get('/parenthome', function(req, res) {
+	console.log('rendering parent home');
+	res.sendFile(path.join(__dirname + '/views/parenthome.html'));
+});
+
+app.get('/home', function(req, res) {
+	console.log('rendering home');
+	res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
 app.get('/', function(req, res) {
@@ -33,4 +52,3 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
 	console.log('== Server is listening on port: ', port);
 });
-
